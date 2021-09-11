720e31a8e8d8f6c84c72ac6ccac3aa85da1d1330
Dellean Santos <dellean.santos@gmail.com>
2021-09-11 01:46:46 -0300

    first test

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import { useState, useCallback } from 'react';
 
 function useFetch(){
+
     const [ loading, setLoading ] = useState(false);
     const [ data, setData ] = useState(null);
     const [ error, setError ] = useState(null);
 
-    const call = (url, options){
-        return fetch(url, options)
+    const call = useCallback((url, options)=>{
+
+            setLoading(true)
+
+            fetch(url, options)
+                .then(setData)
+                .catch(setError)
+                .finally(()=>{ setLoading(false) })
+    }, [])
+
+
+    return {
+        loading,
+        data,
+        error
     }
+
 
 }
 
