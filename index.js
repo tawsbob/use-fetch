1eac3cc61f3bc8559cc7a1422060305c20fa83ae
Julio Lima <julio_flima@hotmail.com>
2021-09-11 16:54:59 -0300

    A small improvement.

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
 import { useState, useCallback } from 'react';
 
 export function useFetch(){
-
     const [ loading, setLoading ] = useState(false);
     const [ response, setResponse ] = useState(null);
     const [ error, setError ] = useState(null);
-
-    const call = useCallback((url, options = { json: false })=>{
-
-            const { json } = options
-
-            setLoading(true)
-
-            if(response){
-                setResponse(null)
-            }
-
-            if(error){
-                setError(null)
-            }
-
-            fetch(url, options)
-                .then((res)=>{
-
-                    if(json){
-                        res
-                        .json()
-                        .then(setResponse)
-                        .catch(setError)
-                    } else {
-                        setResponse(res)
-                    }
-
-                })
-                .catch(setError)
-                .finally(()=>{ setLoading(false) })
+    
+    const reset = useCallback(() => {
+        setLoading(true)
+        setResponse(null)
+        setError(null)
     }, [])
+    
+     const handleResponse = (res) => {
+        if(json)
+            return res
+            .json()
+            .then(setResponse)
+            .catch(setError)
 
+        return setResponse(res)
+    }
+
+    const turnOffLoading = () => setLoading(false)
+
+    const call = useCallback((url, options = { json: false }) = >{
+        const { json } = options
+
+        reset()
+
+        fetch(url, options)
+            .then(handleResponse)
+            .catch(setError)
+            .finally(turnOffLoading)
+    }, [])
 
     return {
         loading,
@@ -44,5 +40,4 @@ export function useFetch(){
         error,
         call
     }
-
 }
